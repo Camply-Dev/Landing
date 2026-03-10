29f2eaacfea52cdc9406f2e151852b44b549a3d5
ADsglt77 <adrien.segalat@gmail.com>
2026-03-10 14:19:41 +0100

    feat(frontend): setup routed skeleton

---
+++ src/components/navBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/services">Services</RouterLink>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
